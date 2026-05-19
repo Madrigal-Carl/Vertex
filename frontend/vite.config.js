2222a2955924072076baacea8d442c8d708f613b
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-05-19 11:56:56 +0800

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/context": path.resolve(__dirname, "./src/context"),
+      "@/hooks": path.resolve(__dirname, "./src/hooks"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+      "@/routes": path.resolve(__dirname, "./src/routes"),
+      "@/services": path.resolve(__dirname, "./src/services"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
+      "@/constants": path.resolve(__dirname, "./src/constants"),
+      "@/store": path.resolve(__dirname, "./src/store"),
+      "@/api": path.resolve(__dirname, "./src/api"),
+    },
+  },
+});
